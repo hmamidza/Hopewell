0f31250d59dbf73fff4f4395f16914fc3986690b
hmamidza <hmamidza@gmail.com>
2026-02-17 02:22:19 +0200

    project created

---
+++ src/main/java/com/example/Hopewell/HopewellApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hopewell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopewellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopewellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hopewell/HopewellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hopewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopewellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
